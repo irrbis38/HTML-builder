93543bd214be0546b833d12e7cdbb1aa4fce7124
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2022-05-23 14:27:04 +0300

    feat: complete task 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,48 @@
+const fs = require("fs");
+const path = require("path");
 
+async function createFolder() {
+  return new Promise(function (resolve, reject) {
+    return fs.mkdir(
+      path.join(__dirname, "files-copy"),
+      { recursive: true },
+      (error) => {
+        if (error) reject(error);
+        resolve();
+      }
+    );
+  });
+}
+
+async function clearDir() {
+  return new Promise(function (resolve, reject) {
+    fs.rm(path.join(__dirname, "files-copy"), { recursive: true }, (error) => {
+      if (error) reject(error);
+      fs.mkdir(path.join(__dirname, "files-copy"), (err) => {
+        if (err) reject(err);
+        resolve();
+      });
+    });
+  });
+}
+
+async function copy() {
+  await createFolder();
+  await clearDir();
+
+  fs.readdir(path.join(__dirname, "files"), (error, files) => {
+    if (error) throw error;
+
+    for (let file of files) {
+      fs.copyFile(
+        path.join(__dirname, "files", file),
+        path.join(__dirname, "files-copy", file),
+        (error) => {
+          if (error) throw error;
+        }
+      );
+    }
+  });
+}
+
+copy();
